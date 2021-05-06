b305a551628d8663e88de364d0179cd1dbbdcbe5
Oleksandr197 <sealanch73@gmai.com>
2021-05-06 05:16:58 +0300

    fetchCoutries

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,7 +1,7 @@
-const BASE_URL = 'https://restcountries.eu/rest/v2/name';
+const BASE_URL = 'https://restcountries.eu/rest/v2';
 
 function fetchCountries(searchQuery) {
-    return fetch(`${BASE_URL}/${searchQuery}`)
+    return fetch(`${BASE_URL}/name/${searchQuery}`)
     .then(response => response.json())     
 };
 
